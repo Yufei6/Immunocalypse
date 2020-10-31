50c9fdb1b0dd78f9f6332f7f32c5be160897f7ea
xiewf2019 <714253971@qq.com>
2020-10-31 21:38:57 +0100

    add attack system

---
+++ Assets/Systems/AutoAttackSystem.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using FYFY;
+
+public class AutoAttackSystem : FSystem {
+	private families virus_f= FamilyManager.getFamily(new AllOfComponents(typeof(Attack),typeof(Move)));
+
+	// Use this to update member variables when system pause. 
+	// Advice: avoid to update your families inside this function.
+	protected override void onPause(int currentFrame) {
+	}
+
+	// Use this to update member variables when system resume.
+	// Advice: avoid to update your families inside this function.
+	protected override void onResume(int currentFrame){
+	}
+
+	// Use to process your families.
+	protected override void onProcess(int familiesUpdateCount) {
+	}
+}
