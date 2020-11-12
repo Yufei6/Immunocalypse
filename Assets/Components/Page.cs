10c17c46207229856ae79a69150aa42a5db83465
xiewf2019 <714253971@qq.com>
2020-11-12 22:22:15 +0100

    collection

---
+++ Assets/Components/Page.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+
+public class Page : MonoBehaviour {
+	public GameObject cam;
+	// Advice: FYFY component aims to contain only public members (according to Entity-Component-System paradigm).
+}
