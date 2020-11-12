b0f64b7c06e89023909dea72fcdd5e957b7db065
xiewf2019 <714253971@qq.com>
2020-11-12 16:46:32 +0100

    hp change

--- Assets/Components/HP.cs
+++ Assets/Components/HP.cs
@@ -1,8 +1,10 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class HP : MonoBehaviour {
 	// Advice: FYFY component aims to contain only public members (according to Entity-Component-System paradigm).
 
 	public int hp = 100;
+	public Slider barre;
 
 }
