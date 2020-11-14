197e05b244a91e8fa25341e9fbeed97d5455ec8a
Yufei <32465277+Yufei6@users.noreply.github.com>
2020-11-15 02:16:16 +0800

    Add some Icon to change CursorStat

---
+++ Assets/Components/TypeCursor.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class TypeCursor : MonoBehaviour {
+	// Advice: FYFY component aims to contain only public members (according to Entity-Component-System paradigm).
+	public Texture2D CursorNormal;
+	public Texture2D CursorM;
+	public Texture2D CursorT;
+	public Texture2D CursorB;
+	public Texture2D CursorDestroy;
+}
