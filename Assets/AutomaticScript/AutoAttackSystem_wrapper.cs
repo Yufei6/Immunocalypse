9b66259bd2a67160d95a2b40ce5004627a1243b0
xiewf2019 <714253971@qq.com>
2020-11-14 16:25:14 +0100

    addbox

---
+++ Assets/AutomaticScript/AutoAttackSystem_wrapper.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using FYFY;
+
+[ExecuteInEditMode]
+public class AutoAttackSystem_wrapper : MonoBehaviour
+{
+	private void Start()
+	{
+		this.hideFlags = HideFlags.HideInInspector; // Hide this component in Inspector
+	}
+
+}
