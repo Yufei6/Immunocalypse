157157ebbce7bb465915ce73864ac544a5cbe2c4
xiewf2019 <714253971@qq.com>
2020-12-05 18:04:54 +0100

    Update AnticorpSystem.cs

--- Assets/Systems/AnticorpSystem.cs
+++ Assets/Systems/AnticorpSystem.cs
@@ -15,5 +15,12 @@ public class AnticorpSystem : FSystem {
 
 	// Use to process your families.
 	protected override void onProcess(int familiesUpdateCount) {
+		foreach (GameObject go in anticorp){
+			float speed = go.GetComponent<Move>().speed;
+			GameObject target=go.GetComponent<Attack>().target;
+			if(Vector3.Distance(go.transform.position, target.transform.position) > 0.001f){
+				go.transform.position = Vector3.MoveTowards(go.transform.position, target.transform.position, speed);
+			}
+		}
 	}
 }
