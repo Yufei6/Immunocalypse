5e8a349dbe235cc153732997cb651a501f9193a9
xiewf2019 <714253971@qq.com>
2021-01-10 18:17:27 +0100

    ss

--- Assets/Systems/AnticorpSystem.cs
+++ Assets/Systems/AnticorpSystem.cs
@@ -18,8 +18,10 @@ public class AnticorpSystem : FSystem {
 		foreach (GameObject go in anticorp){
 			float speed = go.GetComponent<Move>().speed;
 			GameObject target=go.GetComponent<Attack>().target;
-			if(Vector3.Distance(go.transform.position, target.transform.position) > 0.001f){
-				go.transform.position = Vector3.MoveTowards(go.transform.position, target.transform.position, speed*Time.deltaTime);
+			if(target != null){
+				if(Vector3.Distance(go.transform.position, target.transform.position) > 0.001f){
+					go.transform.position = Vector3.MoveTowards(go.transform.position, target.transform.position, speed*Time.deltaTime);
+				}
 			}
 		}
 	}
